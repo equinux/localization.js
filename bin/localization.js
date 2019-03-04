ba8ae93f88bf889f156e98c7143753edcbecfff2
Maximilian Szengel <szengel@equinux.com>
2019-03-04 16:14:09 +0100

    Use @babel/core

--- bin/localization.js
+++ bin/localization.js
@@ -3,7 +3,7 @@
 /*eslint no-console:0 */
 'use strict';
 
-const babel = require('babel-core');
+const babel = require('@babel/core');
 const globSync = require('glob').sync;
 const i18nStringsFiles = require('i18n-strings-files');
 const url = require('url');
