c4f49c4603adf9e1c096040d5dbb8b7409574d92
Maximilian Szengel <szengel@equinux.com>
2025-04-29 17:35:17 +0200

    Skip localization upload if description object contains

--- bin/localization.js
+++ bin/localization.js
@@ -1,60 +1,60 @@
 #!/usr/bin/env node
 
 /*eslint no-console:0 */
-'use strict';
+"use strict";
 
-const babel = require('@babel/core');
-const globSync = require('glob').sync;
-const i18nStringsFiles = require('i18n-strings-files');
-const url = require('url');
-const https = require('https');
-const fs = require('fs');
-const path = require('path');
-const commandLineArgs = require('command-line-args');
+const babel = require("@babel/core");
+const globSync = require("glob").sync;
+const i18nStringsFiles = require("i18n-strings-files");
+const url = require("url");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const commandLineArgs = require("command-line-args");
 
 const options = commandLineArgs([
-  { name: 'command', defaultOption: true },
-  { name: 'base-url', type: String },
-  { name: 'file-pattern', type: String, defaultValue: 'src/**/*.js' },
-  { name: 'pid', type: String },
-  { name: 'loc-version', type: String, defaultValue: '1.0' },
-  { name: 'group', type: String },
-  { name: 'language', type: String, multiple: true },
-  { name: 'upload-language', type: String, defaultValue: 'en' },
-  { name: 'output-path', type: String, defaultValue: 'src/translations' },
-  { name: 'module-source-name', type: String, multiple: true },
-  { name: 'babel-preset', type: String, multiple: true },
-  { name: 'babel-plugin', type: String, multiple: true }
+  { name: "command", defaultOption: true },
+  { name: "base-url", type: String },
+  { name: "file-pattern", type: String, defaultValue: "src/**/*.js" },
+  { name: "pid", type: String },
+  { name: "loc-version", type: String, defaultValue: "1.0" },
+  { name: "group", type: String },
+  { name: "language", type: String, multiple: true },
+  { name: "upload-language", type: String, defaultValue: "en" },
+  { name: "output-path", type: String, defaultValue: "src/translations" },
+  { name: "module-source-name", type: String, multiple: true },
+  { name: "babel-preset", type: String, multiple: true },
+  { name: "babel-plugin", type: String, multiple: true },
 ]);
 
-if (!options['base-url']) {
-  throw new Error('Missing base URL');
+if (!options["base-url"]) {
+  throw new Error("Missing base URL");
 }
 
 if (!options.pid) {
-  throw new Error('Missing PID');
+  throw new Error("Missing PID");
 }
 
 if (!options.pid) {
-  throw new Error('Missing group');
+  throw new Error("Missing group");
 }
 
 if (!options.language) {
-  throw new Error('Missing language');
+  throw new Error("Missing language");
 }
 
-const FILE_PATTERN = options['file-pattern'];
-const OUTPUT_DIR = options['output-path'];
+const FILE_PATTERN = options["file-pattern"];
+const OUTPUT_DIR = options["output-path"];
 const APP_PID = options.pid;
-const LOC_VERSION = options['loc-version'];
+const LOC_VERSION = options["loc-version"];
 const LOC_GROUP = options.group;
-const LANGUAGE_CODE = options['upload-language'];
-const UPLOAD_URL = `${options['base-url']}/uploadStrings.php?pid=${APP_PID}&version=${LOC_VERSION}&groupID=${LOC_GROUP}&language=${LANGUAGE_CODE}`;
-const DOWNLOAD_URL = `${options['base-url']}/getStrings.php?pid=${APP_PID}&version=${LOC_VERSION}&group=${LOC_GROUP}`;
+const LANGUAGE_CODE = options["upload-language"];
+const UPLOAD_URL = `${options["base-url"]}/uploadStrings.php?pid=${APP_PID}&version=${LOC_VERSION}&groupID=${LOC_GROUP}&language=${LANGUAGE_CODE}`;
+const DOWNLOAD_URL = `${options["base-url"]}/getStrings.php?pid=${APP_PID}&version=${LOC_VERSION}&group=${LOC_GROUP}`;
 const LANGUAGES = options.language;
-const BABEL_PRESETS = options['babel-preset'];
-const BABEL_PLUGINS = options['babel-plugin'];
-const MODULE_SOURCE_NAMES = options['module-source-name'] ?? ['formatjs'];
+const BABEL_PRESETS = options["babel-preset"];
+const BABEL_PLUGINS = options["babel-plugin"];
+const MODULE_SOURCE_NAMES = options["module-source-name"] ?? ["formatjs"];
 
 console.dir(
   {
@@ -69,21 +69,21 @@ console.dir(
     LANGUAGES,
     MODULE_SOURCE_NAMES,
     BABEL_PRESETS,
-    BABEL_PLUGINS
+    BABEL_PLUGINS,
   },
   { colors: true }
 );
-console.log('\n');
+console.log("\n");
 
 const args = process.argv;
 if (args.length < 3) {
-  throw new Error('Invalid usage!');
+  throw new Error("Invalid usage!");
 }
 
 const action = args[2];
-if (action === 'upload') {
+if (action === "upload") {
   doUpload();
-} else if (action === 'download') {
+} else if (action === "download") {
   doDownload();
 }
 
@@ -92,33 +92,41 @@ function doUpload() {
 
   // extract messages
   const messages = globSync(FILE_PATTERN)
-    .map(path =>
-      MODULE_SOURCE_NAMES.map(moduleSourceName => {
+    .map((path) =>
+      MODULE_SOURCE_NAMES.map((moduleSourceName) => {
         let messages;
-        const plugins = BABEL_PLUGINS.map(plugin =>
-          plugin === 'formatjs'
+        const plugins = BABEL_PLUGINS.map((plugin) =>
+          plugin === "formatjs"
             ? [
-                'formatjs',
+                "formatjs",
                 {
-                  onMsgExtracted: (_, msg) => (messages = msg)
-                }
+                  onMsgExtracted: (_, msg) => (messages = msg),
+                },
               ]
             : plugin
         );
 
         babel.transformFileSync(path, {
           presets: BABEL_PRESETS,
-          plugins: plugins
+          plugins: plugins,
         });
 
         return messages;
       }).flat()
     )
     .reduce((collection, descriptors) => {
-      descriptors.forEach(descriptor => {
+      descriptors.forEach((descriptor) => {
         const id = descriptor.id;
         const defaultMessage = descriptor.defaultMessage;
         const description = descriptor.description;
+
+        if (
+          typeof description === "object" &&
+          typeof description.skipUpload === "boolean" &&
+          description.skipUpload
+        ) {
+          return;
+        }
 
         if (collection.has(id)) {
           const otherDescriptor = collection.get(id);
@@ -136,7 +144,17 @@ function doUpload() {
           }
         }
 
-        collection.set(id, { defaultMessage, description });
+        const desc = "";
+        if (typeof description === "string") {
+          desc = description;
+        } else if (
+          typeof description === "object" &&
+          typeof description.comment === "string"
+        ) {
+          desc = description.comment;
+        }
+
+        collection.set(id, { defaultMessage, description: desc });
       });
 
       return collection;
@@ -149,42 +167,42 @@ function doUpload() {
 
   messages.forEach((val, key) => {
     const text = val.defaultMessage;
-    const comment = val.description || ' ';
+    const comment = val.description || " ";
 
     data[key] = { text, comment };
   });
 
   // compile strings
   const strings = i18nStringsFiles.compile(data, {
-    encoding: 'utf8',
-    wantsComments: true
+    encoding: "utf8",
+    wantsComments: true,
   });
 
   // upload
   const uploadUrl = url.parse(UPLOAD_URL);
 
   const body =
-    'file=BEGIN\n' + new Buffer(strings).toString('base64') + '\nEND';
+    "file=BEGIN\n" + new Buffer(strings).toString("base64") + "\nEND";
 
   const upload = https.request(
     {
       host: uploadUrl.hostname,
       port: uploadUrl.port,
       path: uploadUrl.path,
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
     },
     function (res) {
-      res.setEncoding('utf8');
-      res.on('data', chunk => {
-        console.log('Changes: ');
-        console.dir(chunk.split('<br><br>').slice(1), { colors: true });
+      res.setEncoding("utf8");
+      res.on("data", (chunk) => {
+        console.log("Changes: ");
+        console.dir(chunk.split("<br><br>").slice(1), { colors: true });
       });
-      res.on('end', () => {
-        console.log('Upload complete.');
+      res.on("end", () => {
+        console.log("Upload complete.");
       });
     }
   );
@@ -196,11 +214,11 @@ function doUpload() {
 
 function doDownload() {
   LANGUAGES.forEach(doDownloadLanguage);
-  console.log('Download complete.');
+  console.log("Download complete.");
 }
 
 function doDownloadLanguage(language) {
-  const downloadUrl = url.parse(DOWNLOAD_URL + '&lang=' + language);
+  const downloadUrl = url.parse(DOWNLOAD_URL + "&lang=" + language);
 
   console.log(`Loading translations for ${language}…`);
 
@@ -209,20 +227,20 @@ function doDownloadLanguage(language) {
       host: downloadUrl.hostname,
       port: downloadUrl.port,
       path: downloadUrl.path,
-      rejectUnauthorized: false
+      rejectUnauthorized: false,
     },
     function (res) {
       var chunks = new String();
 
-      res.setEncoding('utf8');
-      res.on('data', chunk => {
+      res.setEncoding("utf8");
+      res.on("data", (chunk) => {
         chunks += chunk;
       });
 
-      res.on('end', () => {
+      res.on("end", () => {
         const messages = i18nStringsFiles.parse(chunks, {
-          encoding: 'utf8',
-          wantsComments: true
+          encoding: "utf8",
+          wantsComments: true,
         });
         const keys = Object.keys(messages);
         const data = keys.reduce((collection, key) => {
@@ -236,7 +254,7 @@ function doDownloadLanguage(language) {
         const outputPath = path.format({
           dir: OUTPUT_DIR,
           name: language,
-          ext: '.json'
+          ext: ".json",
         });
 
         fs.writeFile(outputPath, JSON.stringify(data, null, 4), function (err) {
@@ -250,4 +268,3 @@ function doDownloadLanguage(language) {
     }
   );
 }
-
